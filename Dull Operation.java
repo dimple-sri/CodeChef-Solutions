85f0e7e1609c2a1efb5dfc53b166b4ec2329166e
Dimple Sri <dimplesree1209@gmail.com>
2026-08-04 22:48:44 +0530

    Add Dull Operation Java program
    Implement basic structure for Dull Operation program.

---
+++ Dull Operation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class CodeChef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            long N = sc.nextLong();
+
+            long x = N;
+            long y = 0;
+
+            System.out.println(x + " " + y);
+        }
+    }
+}
