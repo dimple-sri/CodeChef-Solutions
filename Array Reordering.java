c13271e473addcdb099eee13deedd5ce9d10bfa0
Dimple Sri <dimplesree1209@gmail.com>
2026-08-18 22:08:10 +0530

    Implement array reordering and sorting
    This program reads two arrays, sorts them, and prints them in descending order for the first array and ascending order for the second.

---
+++ Array Reordering.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0)
+		{
+		    int n = sc.nextInt();
+		    int a[] = new int[n];
+		    int b[] = new int[n];
+		    
+		    for (int i = 0; i < n; i++)
+		    {
+		        a[i] = sc.nextInt();    
+		    }
+		    for (int i = 0; i < n; i++)
+		    {
+                b[i] = sc.nextInt();
+		    }
+		    Arrays.sort(a);
+		    Arrays.sort(b);
+		    for(int i = n-1; i >= 0; i--)
+		    {
+		        System.out.print(a[i] + " ");
+		        System.out.println();
+		    }
+		    for (int i = 0; i < n; i++)
+		    {
+		        System.out.print(b[i] + " ");
+		        System.out.println();
+		    }
+		    
+		    
+		}
+
+	}
+}
